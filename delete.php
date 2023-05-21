66c62501d8d2d26b23e780aa3a7d3381e3480a6e
haml3urger <subhamits4.2@gmail.com>
2023-05-21 17:38:11 +0545

    after deleteing goes back to index.php

--- delete.php
+++ delete.php
@@ -1,10 +1,11 @@
 <?php
 include_once('config.php');
-$id=$_GET['id'];
-$result =mysqli_query($con,"delete from user where id='$id';");
-if(!$result) die("Failed to delete: ".mysqli_error($con));
-echo "<font color='green'>Successfully Deleted";
-echo "<br/><a href='index.php'>View Result</a>";
-
-
+$id = $_GET['id'];
+$result = mysqli_query($con, "DELETE FROM user WHERE id='$id';");
+if (!$result) {
+    die("Failed to delete: " . mysqli_error($con));
+} else {
+    echo "<script>alert('Data deleted successfully');</script>";
+    echo "<script>window.location.href = 'index.php';</script>";
+}
 ?>
